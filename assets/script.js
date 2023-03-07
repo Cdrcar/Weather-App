23a00740bf97e58451f726fe6fb5ec2d99901405
Carmela <carmela881@outlook.com>
2023-03-07 14:41:46 +0000

    added style to search history container

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@
 var apiKey = "1b18ce13c84e21faafb19c931bb29331";
 var savedSearches = [];
 
+
+
 //Event listener when click on the button from the search form 
 $(".btn").on("click", function(event) {
     event.preventDefault();
@@ -22,3 +24,37 @@ $(".btn").on("click", function(event) {
         fiveDayForecastSection(cityName);
     }
 });
+
+
+
+// Create list of searched cities
+var searchHistoryList = function(cityName) {
+    $('.past-search:contains("' + cityName + '")').remove();
+
+    // City entry: create a <p> element with a "past-search" class and add City Name as text content
+    var searchHistoryEntry = $("<p>");
+    searchHistoryEntry.addClass("past-search");
+    searchHistoryEntry.text(cityName);
+
+    // Container for City entry: create <div> element with a "past-search-container" class and append City entry to the Container
+    var searchEntryContainer = $("<div>");
+    searchEntryContainer.addClass("past-search-container");
+    searchEntryContainer.append(searchHistoryEntry);
+
+    // Append Container for City to Search History Container Element
+    var searchHistoryContainerEl = $("#search-history-container");
+    searchHistoryContainerEl.append(searchEntryContainer);
+
+    // Add city name to array of savedSearches and setItem to Local Storage
+      savedSearches.push(cityName);
+      localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+
+    if (savedSearches.length > 0){
+    // Update savedSearches array with previously saved searches
+        var previousSavedSearches = localStorage.getItem("savedSearches");
+        savedSearches = JSON.parse(previousSavedSearches);
+    }
+
+    // Reset search input
+    $("#search-input").val("");
+};

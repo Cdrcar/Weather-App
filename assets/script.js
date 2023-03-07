15e129843eabf2392271d0ccd9a289b7d84aabbc
Carmela <carmela881@outlook.com>
2023-03-07 13:45:48 +0000

    added event listener when click on search botton

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
 // Variables
 var apiKey = "1b18ce13c84e21faafb19c931bb29331";
 var savedSearches = [];
+
+//Event listener when click on the button from the search form 
+$(".btn").on("click", function(event) {
+    event.preventDefault();
+    
+    // Get city searched
+
+    var cityName = $("#search-input").val();
+
+    //If no city entered, send alert to enter a name of city
+
+    if (cityName === "" || cityName == null) {
+        //send alert if search input is empty when submitted
+        alert("Please enter name of city.");
+        event.preventDefault();
+    } else {
+        // If cityName is true (valid), display its current weather conditions and the forecast weather and the cityName it will be added to the search history list
+        currentWeatherSection(cityName);
+        fiveDayForecastSection(cityName);
+    }
+});
